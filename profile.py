2b0f0d9ffbd87e364651c5cb5933e0e27702f85b
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-02-05 16:20:09 -0700

    Update profile.py
    Added shell from previous owk8s repo.

--- profile.py
+++ profile.py
@@ -1,23 +1,48 @@
 """
-First attempt taken from https://github.com/emulab/my-profile/blob/master/profile.py to test the set up
+TODO: write directions.
 """
 
 # Import the Portal object.
 import geni.portal as portal
 # Import the ProtoGENI library.
-import geni.rspec.pg as pg
+import geni.rspec.pg as rspec
 
-# Create a portal context.
+request = portal.context.makeRequestRSpec()
+
+# Set up parameters
 pc = portal.Context()
+pc.defineParameter("nodeCount", 
+                   "Number of nodes in the experiment. It is recommended that at least 3 be used for the Kubernetes/OpenWhisk ",
+                   portal.ParameterType.INTEGER, 
+                   3
+)
+pc.defineParameter("nodeType", 
+                   "Node Hardware Type",
+                   portal.ParameterType.NODETYPE, 
+                   "m510",
+                   longDescription="A specific hardware type to use for all nodes. This profile has primarily been tested with m510 and xl170 nodes.")
+params = pc.bindParameters()
 
-# Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.RawPC("node")
+# Verify parameters
+if params.nodeCount > 10:
+    perr = portal.ParameterWarning("Do you really need more than 8 compute nodes?  Think of your fellow users scrambling to get nodes :).",['nodeCount'])
+    pc.reportWarning(perr)
+    pass
+pc.verifyParameters()
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+link_members = []
 
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+# Create nodes
+for i in range(params.nodeCount):
+    node = request.RawPC("node"+str(i+1))
+    node.disk_image = 'urn:publicid:IDN+utah.cloudlab.us+image+cu-bison-lab-PG0:openwhisk:1'
+    node.hardware_type = params.nodeType
+    link_members.append(node)
+
+    # Install and execute a script that is contained in the repository.
+    node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+    
+# Create a link between nodes
+link1 = request.Link(members = link_members)
+
+portal.context.printRequestRSpec()
