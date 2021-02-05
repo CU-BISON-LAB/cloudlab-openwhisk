b637882ec99e2b6a3939b8cc7901d0d0b07a3a26
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-02-05 14:29:44 -0700

    Create image_setup.sh
    Added script to document image setup

---
+++ scripts/image_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+
+# Install Kubernetes
+sudo apt-get update && apt-get install -y apt-transport-https curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Download and install Helm
+# Note that OpenWhisk now requires Helm 3 or above
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+sudo ./get_helm.sh
+
+# Download and install the OpenWhisk CLI
+wget https://github.com/apache/openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz
+tar -xvf OpenWhisk_CLI-latest-linux-386.tgz
+sudo mv wsk /usr/local/bin/wsk
+
+# Clone the Kubernetes OpenWhisk deployment repo, and move your yaml file to the
+# correct location.
+git clone https://github.com/apache/openwhisk-deploy-kube.git
+mv mycluster.yaml openwhisk-deploy-kube/mycluster.yaml
+
+# Packages needed for standalone openwhisk build
+sudo apt install -y nodejs npm default-jre
+
+# Needed for OpenWhisk tests
+sudo apt install -y default-jdk
