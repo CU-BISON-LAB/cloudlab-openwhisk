d52b83c52a0e31bef3849a9b6579fb30a2162e45
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-02-05 14:55:51 -0700

    Update image_setup.sh
    Updated method of installing Kubernetes based on Ubuntu 20.04 instructions

--- scripts/image_setup.sh
+++ scripts/image_setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # Install docker
-sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt update
+sudo apt install -y docker.io
 
 # Install Kubernetes
-sudo apt-get update && apt-get install -y apt-transport-https curl
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+# From: https://linuxconfig.org/how-to-install-kubernetes-on-ubuntu-20-04-focal-fossa-linux
+sudo apt install apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt install -y kubeadm kubelet kubectl
+sudo apt-mark hold kubeadm kubelet kubectl
 
 # Download and install Helm
 # Note that OpenWhisk now requires Helm 3 or above
