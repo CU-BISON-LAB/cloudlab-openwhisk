1d4de2611f2773dde90ecfef29c79730ccb6eb40
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2021-02-14 19:42:47 -0700

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -4,3 +4,7 @@
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# set up wsk
+wsk property set --apihost 10.10.1.1:31001
+wsk property set --auth 23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
