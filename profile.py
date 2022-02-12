60bd36172522e27c786175407b05d3dc16c87e3b
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-02-12 16:50:04 -0600

    Fix stderr redirect bug in profile.py

--- profile.py
+++ profile.py
@@ -94,11 +94,11 @@ for i in range(params.nodeCount):
 
 # Iterate over secondary nodes first
 for i, node in enumerate(nodes[1:]):
-    node.addService(rspec.Execute(shell="bash", command="/local/repository/start.sh secondary {}.{} {} 2>&1 > /home/cloudlab-openwhisk/start.log &".format(
+    node.addService(rspec.Execute(shell="bash", command="/local/repository/start.sh secondary {}.{} {} > /home/cloudlab-openwhisk/start.log 2>&1 &".format(
       BASE_IP, i + 2, params.startKubernetes)))
 
 # Start primary node
-nodes[0].addService(rspec.Execute(shell="bash", command="/local/repository/start.sh primary {}.1 {} {} {} 2>&1 > /home/cloudlab-openwhisk/start.log".format(
+nodes[0].addService(rspec.Execute(shell="bash", command="/local/repository/start.sh primary {}.1 {} {} {} > /home/cloudlab-openwhisk/start.log 2>&1".format(
   BASE_IP, params.nodeCount, params.startKubernetes, params.deployOpenWhisk)))
 
 
