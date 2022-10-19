d05cc02de683850a973bed6efd31b1f00adee93a
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-10-19 14:49:21 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,7 +16,7 @@ import geni.rspec.pg as rspec
 
 BASE_IP = "10.10.1"
 BANDWIDTH = 10000000
-IMAGE = 'urn:publicid:IDN+apt.emulab.net+image+cu-bison-lab-PG0:openwhiskv3'
+IMAGE = 'urn:publicid:IDN+apt.emulab.net+image+cu-bison-lab-PG0:openwhiskv3:0'
 
 # Set up parameters
 pc = portal.Context()
