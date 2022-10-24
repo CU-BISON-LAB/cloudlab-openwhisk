c3bfe80507c54ab5067a9e4965d2f7b8bf146d6d
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-10-24 16:23:47 -0600

    Update image_setup.sh

--- image_setup.sh
+++ image_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
 
+# Use particular docker and kubernetes versions. When I've tried to upgrade, I've seen slowdowns in 
+# pod creation.
+DOCKER_VERSION_STRING=5:20.10.12~3-0~ubuntu-focal
+KUBERNETES_VERSION_STRING=1.23.3-00
+
 # Unlike home directories, this directory will be included in the image
 OW_USER_GROUP=owuser
 INSTALL_DIR=/home/cloudlab-openwhisk
@@ -31,7 +36,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce=$DOCKER_VERSION_STRING docker-ce-cli=$DOCKER_VERSION_STRING containerd.io docker-compose-plugin
 
 # Set to use cgroupdriver
 echo -e '{
@@ -49,11 +54,10 @@ sudo docker run hello-world | grep "Hello from Docker!" || (echo "ERROR: Docker 
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt install -y kubelet=$KUBERNETES_VERSION_STRING kubeadm=$KUBERNETES_VERSION_STRING kubectl=$KUBERNETES_VERSION_STRING
 
 # Set to use private IP
 sudo sed -i.bak "s/KUBELET_CONFIG_ARGS=--config=\/var\/lib\/kubelet\/config\.yaml/KUBELET_CONFIG_ARGS=--config=\/var\/lib\/kubelet\/config\.yaml --node-ip=REPLACE_ME_WITH_IP/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-sudo sed -i.bak '6 i Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 # Download and install the OpenWhisk CLI
 wget https://github.com/apache/openwhisk-cli/releases/download/latest/OpenWhisk_CLI-latest-linux-386.tgz
@@ -75,8 +79,4 @@ sudo chmod -R o+rw $INSTALL_DIR
 git clone https://github.com/apache/openwhisk-deploy-kube $INSTALL_DIR/openwhisk-deploy-kube
 sudo chgrp -R $OW_USER_GROUP $INSTALL_DIR
 sudo chmod -R o+rw $INSTALL_DIR
-
-# Got from similar issue here: https://github.com/containerd/containerd/issues/4581
-sudo rm /etc/containerd/config.toml
-sudo systemctl restart containerd 
 
